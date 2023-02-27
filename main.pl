d9ebba9d52330c98c5b8611ebf385e664dea94a0
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 20:50:31 -0800

    test without helper

--- main.pl
+++ main.pl
@@ -31,21 +31,16 @@ simplify(E ^ 0, 1).
 simplify(E ^ 1, E).
 simplify(0 ^ E, 0).
 
-
 simplify(E + Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE + EvalY, S).
 simplify(E - Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE - EvalY, S).
 simplify(E * Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE * EvalY, S).
 simplify(E / Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE / EvalY, S).
 simplify(E ^ Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE ^ EvalY, S).
 
-% simplify(E + Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE + EvalY.
-% simplify(E - Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE - EvalY.
-% simplify(E * Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE * EvalY.
-% simplify(E / Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE / EvalY.
-% simplify(E ^ Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE ^ EvalY.
 
-simplify_helper(E, E) :- integer(E).
-simplify_helper(E, E) :- atom(E).
+% simplify_helper(E, E) :- integer(E).
+% simplify_helper(E, E) :- atom(E).
+
 simplify_helper(E - E, 0).
 simplify_helper(E - 0, E).
 simplify_helper(E + 0, E).
@@ -61,17 +56,12 @@ simplify_helper(E / 1, E).
 simplify_helper(E ^ 0, 1).
 simplify_helper(E ^ 1, E).
 simplify_helper(0 ^ E, 0).
+
 simplify_helper(E + Y, E + Y).
 simplify_helper(E - Y, E - Y).
 simplify_helper(E * Y, E * Y).
 simplify_helper(E / Y, E / Y).
 simplify_helper(E ^ Y, E ^ Y).
-
-% simplify(E ^ Y, EvalE ^ EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-% simplify(E * Y, EvalE * EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-% simplify(E / Y, EvalE / EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-% simplify(E + Y, EvalE + EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-% simplify(E - Y, EvalE - EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
 
 
 
