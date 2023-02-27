a647a6a437d6c954a9590ce14277b2a972fc229c
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 20:10:10 -0800

    PEMDAS

--- main.pl
+++ main.pl
@@ -27,36 +27,6 @@ simplify(E ^ 0, 1).
 simplify(E ^ 1, E).
 
 
-% simplify(E + Y, SimpleE + Y) :- simplify(E, SimpleE), atom(Y).
-% simplify(E + Y, E + SimpleY) :- simplify(Y, SimpleY), atom(E).
-
-% simplify(E - Y, SimpleE - Y) :- simplify(E, SimpleE), atom(Y).
-% simplify(E - Y, E - SimpleY) :- simplify(Y, SimpleY), atom(E).
-
-% simplify(E * Y, SimpleE * Y) :- simplify(E, SimpleE), atom(Y).
-% simplify(E * Y, E * SimpleY) :- simplify(Y, SimpleY), atom(E).
-
-% simplify(E / Y, SimpleE / Y) :- simplify(E, SimpleE), atom(Y).
-% simplify(E / Y, E / SimpleY) :- simplify(Y, SimpleY), atom(E).
-
-% simplify(E ^ Y, SimpleE ^ Y) :- simplify(E, SimpleE), atom(Y).
-% simplify(E ^ Y, E ^ SimpleY) :- simplify(Y, SimpleY), atom(E).
-
-% simplify(E + Y, E + Y) :- integer(E), atom(Y).
-% simplify(E + Y, E + Y) :- integer(Y), atom(E).
-
-% simplify(E - Y, E - Y) :- integer(E), atom(Y).
-% simplify(E - Y, E - Y) :- integer(Y), atom(E).
-
-% simplify(E * Y, E * Y) :- integer(E), atom(Y).
-% simplify(E * Y, E * Y) :- integer(Y), atom(E).
-
-% simplify(E / Y, E / Y) :- integer(E), atom(Y).
-% simplify(E / Y, E / Y) :- integer(Y), atom(E).
-
-% simplify(E ^ Y, E ^ Y) :- integer(E), atom(Y).
-% simplify(E ^ Y, E ^ Y) :- integer(Y), atom(E).
-
 % simplify(E + Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE + EvalY.
 % simplify(E - Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE - EvalY.
 % simplify(E * Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE * EvalY.
@@ -69,14 +39,12 @@ simplify(E ^ 1, E).
 % simplify(E / Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify(EvalE / EvalY, S).
 % simplify(E ^ Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify(EvalE ^ EvalY, S).
 
-
-simplify(E + Y, EvalE + EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-simplify(E - Y, EvalE - EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
+simplify(E ^ Y, EvalE ^ EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
 simplify(E * Y, EvalE * EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
 simplify(E / Y, EvalE / EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
-simplify(E ^ Y, EvalE ^ EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
+simplify(E + Y, EvalE + EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
+simplify(E - Y, EvalE - EvalY) :- simplify(E, EvalE), simplify(Y, EvalY).
 
-% simplify(E + Y, S) :- integer(E), atom(Y), eval(E, EvalE), S is (EvalE + Y).
 
 
 % deriv(E,D) to do symbolic differentiation of polynomial arithmetic expressions with respect to x.
