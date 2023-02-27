897b7589c00f0c4f4915c44b41a2c6fcda22ae81
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 20:48:26 -0800

    test

--- main.pl
+++ main.pl
@@ -18,13 +18,18 @@ simplify(E, E) :- atom(E).
 simplify(E - E, 0).
 simplify(E - 0, E).
 simplify(E + 0, E).
+simplify(0 - E, E).
+simplify(0 + E, E).
 simplify(0 / E, 0).
 simplify(E * 0, 0).
+simplify(0 * E, 0).
 simplify(E * 1, E).
+simplify(1 * E, E).
 simplify(E / E, 1).
 simplify(E / 1, E).
 simplify(E ^ 0, 1).
 simplify(E ^ 1, E).
+simplify(0 ^ E, 0).
 
 
 simplify(E + Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(EvalE + EvalY, S).
@@ -39,18 +44,23 @@ simplify(E ^ Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), simplify_helper(Ev
 % simplify(E / Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE / EvalY.
 % simplify(E ^ Y, V) :- simplify(E, EvalE), simplify(Y, EvalY), V is EvalE ^ EvalY.
 
-simplify(E, E) :- integer(E).
-simplify(E, E) :- atom(E).
+simplify_helper(E, E) :- integer(E).
+simplify_helper(E, E) :- atom(E).
 simplify_helper(E - E, 0).
 simplify_helper(E - 0, E).
 simplify_helper(E + 0, E).
+simplify_helper(0 - E, E).
+simplify_helper(0 + E, E).
 simplify_helper(0 / E, 0).
 simplify_helper(E * 0, 0).
+simplify_helper(0 * E, 0).
 simplify_helper(E * 1, E).
+simplify_helper(1 * E, E).
 simplify_helper(E / E, 1).
 simplify_helper(E / 1, E).
 simplify_helper(E ^ 0, 1).
 simplify_helper(E ^ 1, E).
+simplify_helper(0 ^ E, 0).
 simplify_helper(E + Y, E + Y).
 simplify_helper(E - Y, E - Y).
 simplify_helper(E * Y, E * Y).
