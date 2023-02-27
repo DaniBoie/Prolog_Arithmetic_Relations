8a0384ec8b161af2b5c5d745e357a45bcb1f246f
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 19:05:21 -0800

    simplify test

--- main.pl
+++ main.pl
@@ -11,16 +11,20 @@ eval(E ^ Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE ^ EvalY.
 
 
 % simplify(E,S) that simplifies polynomial arithmetic expressions involving constants, variables (which are Prolog atoms that start with a lowercase letter), and operators +,-,*,/,^.
-
+simplify(E, E) :- integer(E).
 simplify(E, E) :- atom(E).
+simplify(E + Y, E + Y) :- integer(E), atom(Y).
+
 simplify(E - E, 0) :- atom(E).
 simplify(E / E, 1) :- atom(E).
 simplify(E ^ 0, 1) :- atom(E).
 simplify(E ^ 1, E) :- atom(E).
 
+simplify(E * Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), S is EvalE * EvalY.
+simplify(E / Y, S) :- simplify(E, EvalE), simplify(Y, EvalY), S is EvalE / EvalY.
 
 
-%simplify(E + Y, S) :- integer(E), atom(Y), eval(E, EvalE), S is (EvalE + Y).
+% simplify(E + Y, S) :- integer(E), atom(Y), eval(E, EvalE), S is (EvalE + Y).
 
 
 % deriv(E,D) to do symbolic differentiation of polynomial arithmetic expressions with respect to x.
