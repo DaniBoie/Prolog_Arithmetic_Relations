f4533d0ec63335fd400441559c17e15f45ab2665
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 18:32:07 -0800

    test with all

--- main.pl
+++ main.pl
@@ -1,4 +1,9 @@
 
 eval(E, E):- integer(E).
 eval(E + Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE + EvalY.
+eval(E - Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE - EvalY.
+
 eval(E * Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE * EvalY.
+eval(E / Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE / EvalY.
+
+eval(E ^ Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE ^ EvalY.
