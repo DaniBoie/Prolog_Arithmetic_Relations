4d1b0be910f7f1972a1a51c609dc0413eeb5c1a4
Daniel Ayala <dannyboy3385@gmail.com>
2023-02-26 18:40:43 -0800

    simplify first test

--- main.pl
+++ main.pl
@@ -1,3 +1,4 @@
+% eval(E,V) that evaluates integer arithmetic expressions consisting of integer constants and operators +,-,*,/,^ to a constant value.
 
 eval(E, E):- integer(E).
 eval(E + Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE + EvalY.
@@ -7,3 +8,12 @@ eval(E * Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE * EvalY.
 eval(E / Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE / EvalY.
 
 eval(E ^ Y, V) :- eval(E, EvalE), eval(Y, EvalY), V is EvalE ^ EvalY.
+
+
+% simplify(E,S) that simplifies polynomial arithmetic expressions involving constants, variables (which are Prolog atoms that start with a lowercase letter), and operators +,-,*,/,^.
+
+simplify(E, E) :- atom(E).
+
+
+% deriv(E,D) to do symbolic differentiation of polynomial arithmetic expressions with respect to x.
+
